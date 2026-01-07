7f03ad01b9ed2ab1803a7853a18883ac029325e6
Josback <backelantsjoseph@gmail.com>
2026-01-07 12:02:10 +0100

    ajout d'un api service

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,19 +1,22 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = "https://fakestoreapi.com/products";
+  final String baseUrl = "https://fakestoreapi.com/";
 
   Future<List<dynamic>> fetchProducts() async {
     try {
-      final response = await http.get(Uri.parse(baseUrl));
+      final response = await http.get(Uri.parse("${baseUrl}products"));
+
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         return data;
       } else {
-        throw Exception("Erreur serveur  : ${response.statusCode}");
+        throw Exception("Erreur lors de la récupération des produits");
       }
     } catch (e) {
+      print("Voci l'exception : $e");
       return [];
     }
   }
